9b73fb90c7a2762069be50b2897afbbd2fa00cc1
mdasif200112 <mdasif200112@gmail.com>
2022-03-26 19:53:07 +0600

    firstjs

---
+++ script.js
@@ -0,0 +1,16 @@
+var num = 20;
+console.log(typeof(num));
+num = toString(num);
+console.log(typeof(num));
+var num1 = '2l';
+num1 = parseInt(num1);
+console.log(typeof(num1));
+
+var num2 = '200.34343';
+num2 = parseFloat(num2);
+console.log(typeof(num2));
+
+var number = 2.434343242;
+console.log(number.toFixed(10));
+
+
