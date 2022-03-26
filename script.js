74798b6983e03af26e127b77c0eed0a7aa86af26
mdasif200112 <mdasif200112@gmail.com>
2022-03-26 20:08:36 +0600

    addthis

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var num = 20;
 console.log(typeof(num));
+
 num = toString(num);
 console.log(typeof(num));
+
 var num1 = '2l';
 num1 = parseInt(num1);
 console.log(typeof(num1));
@@ -11,6 +13,13 @@ num2 = parseFloat(num2);
 console.log(typeof(num2));
 
 var number = 2.434343242;
-console.log(number.toFixed(10));
+console.log(number.toFixed(1));
+ 
+var number = 2.3453423432;
+console.log(number.toPrecision(1));
+
+console.log(typeof(Number('12')));
+
+console.log(Number(true));
 
 
