95ce382751345e8cc103d9da3ea7e1493b0a787b
mdasif200112 <mdasif200112@gmail.com>
2022-03-27 21:35:28 +0600

    addthis

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-var num = 20;
-console.log(typeof(num));
+var text = "bangladesh";
+var len = text.length;
+document.write("Number of character " + len);
 
-num = toString(num);
-console.log(typeof(num));
+var text = "pakistan";
+document.write("<br>" + 'number of charecter ' +  text.length);
 
-var num1 = '2l';
-num1 = parseInt(num1);
-console.log(typeof(num1));
+var text = prompt("Enter your name : ")
+document.write("<br>" + 'Number of character ' + text.length);
 
-var num2 = '200.34343';
-num2 = parseFloat(num2);
-console.log(typeof(num2));
+var text = "Bangladesh";
+document.write("<br>" + text.charAt(2));
 
-var number = 2.434343242;
-console.log(number.toFixed(1));
- 
-var number = 2.3453423432;
-console.log(number.toPrecision(1));
-
-console.log(typeof(Number('12')));
-
-console.log(Number(true));
-
+var text = 'bangladesh';
+text = text.toUpperCase();
+document.write("<br>" + text);
 
