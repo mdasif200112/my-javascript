c30ee8c7a74e3c95aab299068f261ebb0dfb70a7
mdasif200112 <mdasif200112@gmail.com>
2022-03-27 23:29:16 +0600

    addthis

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ var text = 'bangladesh';
 text = text.toUpperCase();
 document.write("<br>" + text);
 
+var text = "Bangladesh";
+text = text.toLowerCase();
+document.write("<br>" + text);
+
+var text1 = "Bangladesh ";
+var text2 = 'is a beautiful country';
+document.write("<br>" + text1.concat(text2));
+
+var text3 = "Bangladesh ";
+var text4 = "is a beautiful country."
+var text5 = text3.concat(text4);
+document.write("<br>" + text5);
+
+var text1 = "Bangladesh ";
+var text = text1.slice(0, 6);
+document.write("<br>" + text);
