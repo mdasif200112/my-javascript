23360d212521024abf603bba9061392ead1668a5
mdasif200112 <mdasif200112@gmail.com>
2022-03-29 12:37:32 +0600

    add

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-var baseA = parseFloat(prompt("Enter Base A: "));
-var baseB = parseFloat(prompt("Enter Base B: "));
-var height = parseFloat(prompt("Enter Height: "));
- 
-var area =( (baseA + baseB)*height)/2;
-document.write("Area = " + area);
+var num = -55;
+var result = Math.abs(num);
+console.log(result);
+
+var num = 4343.33434343;
+var result = Math.round(num);
+console.log(num);
+
+var num = 43.55;
+var result = Math.ceil(num);
+console.log(result);
+
+var num = 3.0004;
+var result = Math.floor(num);
+console.log(result);
+
+var num = 3.34434;
+var result = Math.random(num);
+console.log(result);
+
+var num = Math.random() * 10;
+var result =Math.round(num);
+console.log(result);
+
+var num = Math.random() * 10;
+var result = num.toFixed(3);
+console.log(result);
+
+var num = Math.random() * 10;
+var result = num.toPrecision(1);
+console.log(result);
--- scriptshape.js
+++
@@ -1,6 +0,0 @@
-var baseA = parseFloat(prompt("Enter Base A: "));
-var baseB = parseFloat(prompt("Enter Base B: "));
-var height = parseFloat(prompt("Enter Height: "));
- 
-var area =( (baseA + baseB)*height)/2;
-document.write("Area = " + area);
